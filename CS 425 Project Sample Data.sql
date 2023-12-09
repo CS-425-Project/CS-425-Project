fd6aef0cb21aeaed57b405a90cb20e678a30d815
Hamza-Syed <113389212+Hamza-Syed@users.noreply.github.com>
2023-12-09 09:55:10 -0600

    Update CS 425 Project Sample Data.sql

--- CS 425 Project Sample Data.sql
+++ CS 425 Project Sample Data.sql
@@ -2,13 +2,13 @@ INSERT INTO Card (card_name, card_type, hp, rarity, move1, move2, card_id)
 VALUES ('Card Name','Card Type',200,'Very Rare','Move 1','Move 2',1234);
 
 INSERT INTO Pack (set_name, pack_id, price)
-VALUES ('Set Name',1324,50);
+VALUES ('Set Name',1234,50);
 
 INSERT INTO Store(store_id, store_name, total_packs, packs_sold)
-VALUES (1243,'Store Name',100,80);
+VALUES (1234,'Store Name',100,80);
 
 INSERT INTO Factory(factory_id, factory_name, packs_made)
-VALUES (1342,'Factory Name',200);
+VALUES (1234,'Factory Name',200);
 
 INSERT INTO Employee(employee_id, factory_name, emp_name, salary)
-VALUES (1432,'Factory Name','Employee Name',100000)
+VALUES (1234,'Factory Name','Employee Name',100000)
